4528c106b8c858c8f2dccde42a8d41e437361ea1
izzy417 <israeldail2@gmail.com>
2025-01-22 19:59:26 +0000

    removed file

--- portfolio/app/root.tsx
+++ portfolio/app/root.tsx
@@ -8,7 +8,6 @@ import {
 import type { LinksFunction } from "@remix-run/node";
 
 import "./tailwind.css";
-import appStyles from './appStyles.css';
 
 export const links: LinksFunction = () => [
   { rel: "preconnect", href: "https://fonts.googleapis.com" },
@@ -21,10 +20,6 @@ export const links: LinksFunction = () => [
     rel: "stylesheet",
     href: "https://fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&display=swap",
   },
-  {
-    rel: "stylesheet",
-    href: appStyles,
-  }
 ];
 
 export function Layout({ children }: { children: React.ReactNode }) {
