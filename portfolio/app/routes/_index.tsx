4564b0d4abce778e7a0eca397848792cc748bce0
izzy417 <israeldail2@gmail.com>
2025-01-23 18:14:02 +0000

    changed text to black for better contrast

--- portfolio/app/routes/_index.tsx
+++ portfolio/app/routes/_index.tsx
@@ -12,19 +12,19 @@ export default function Index() {
     <div className="flex flex-col gap-4 items-center mx-auto w-[calc(100%-40px)] p-6 md:p-12 md:flex-row md:justify-center md:flex-wrap">
       { /* TODO: add meaningful content - short bio of myself, highlight skills, highlight current educational path,
            highlight done or on-going projects, link to contact form  */ }
-      <div className="bg-[#a28089] text-white rounded p-6 w-full transform hover:-translate-y-[10%] hover:shadow-lg transition duration-500 ease-in-out md:basis-[28%] md:grow">
+      <div className="bg-[#a28089] text-[#000] rounded p-6 w-full transform hover:-translate-y-[10%] hover:shadow-lg transition duration-500 ease-in-out md:basis-[28%] md:grow">
         bio goes here
       </div>
 
-      <div className="bg-[#a28089] text-white rounded p-6 w-full transform hover:-translate-y-[10%] hover:shadow-lg transition duration-500 ease-in-out md:basis-[68%] md:grow">
+      <div className="bg-[#a28089] text-[#000] rounded p-6 w-full transform hover:-translate-y-[10%] hover:shadow-lg transition duration-500 ease-in-out md:basis-[68%] md:grow">
         education progress goes here
       </div>
 
-      <div className="bg-[#a28089] text-white rounded p-6 w-full transform hover:-translate-y-[10%] hover:shadow-lg transition duration-500 ease-in-out md:basis-[48%] md:grow">
+      <div className="bg-[#a28089] text-[#000] rounded p-6 w-full transform hover:-translate-y-[10%] hover:shadow-lg transition duration-500 ease-in-out md:basis-[48%] md:grow">
         skills go here
       </div>
 
-      <div className="bg-[#a28089] text-white rounded p-6 w-full transform hover:-translate-y-[10%] hover:shadow-lg transition duration-500 ease-in-out md:basis-[48%] md:grow">
+      <div className="bg-[#a28089] text-[#000] rounded p-6 w-full transform hover:-translate-y-[10%] hover:shadow-lg transition duration-500 ease-in-out md:basis-[48%] md:grow">
         complete/on-going projects go here
       </div>
     </div>
