dbe8d42da54f374e1788d57952442953f6c0310d
izzy417 <israeldail2@gmail.com>
2025-01-23 16:39:20 +0000

    time to add some content later

--- portfolio/app/routes/_index.tsx
+++ portfolio/app/routes/_index.tsx
@@ -9,10 +9,14 @@ export const meta: MetaFunction = () => {
 
 export default function Index() {
   return (
-    <div className="flex flex-col items-center mx-auto w-[calc(100%-40px)] p-12 md:flex-row md:justify-center">
+    <div className="flex flex-col gap-4 items-center mx-auto w-[calc(100%-40px)] p-12 md:flex-row md:justify-center">
       { /* TODO: add meaningful content - short bio of myself, highlight skills, highlight current educational path,
            highlight done or on-going projects, link to contact form  */ }
-      <div className="bg-[#a28089] rounded p-6 self-start md:mr-auto">
+      <div className="bg-[#a28089] rounded p-6 w-full md:grow">
+        content
+      </div>
+
+      <div className="bg-[#a28089] rounded p-6 w-full md:grow">
         content
       </div>
     </div>
