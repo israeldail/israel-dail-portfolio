f57dfa83e175e75b2236562c13f4f8ed1470212f
israeldail <israeldail2@gmail.com>
2025-01-23 23:40:06 -0500

    updated navbar and placeholder for work section

--- portfolio/app/routes/about.tsx
+++
@@ -1,7 +0,0 @@
-export default function About() {
-  return (
-    <div className="text-center p-10">
-      boilerplate
-    </div>
-  );
-}
--- portfolio/app/routes/work.tsx
+++ portfolio/app/routes/work.tsx
@@ -1,7 +1,10 @@
 export default function Work() {
   return (
-    <div className="text-center p-10">
-      boilerplate
+    <div className="flex flex-col gap-4 items-center mx-auto max-w-[1200px] w-[calc(100%-40px)] p-8 lg:gap-8 lg:p-12 lg:flex-row lg:justify-center lg:flex-wrap">
+      <div className="flex items-center bg-[#a28089] p-4 min-w-[200px] w-full text-[#1E1E1E] rounded text-sm shadow-md border border-[#000000] border-opacity-20 md:text-base lg:p-6 lg:basis-[100%] lg:grow">
+        <i className="fa-solid fa-circle-info fa-2x text-[#51e2f5]"></i>
+        <p className="text-sm md:text-base px-4">Under construction... will update as soon as possible.</p>
+      </div>
     </div>
   );
 }
