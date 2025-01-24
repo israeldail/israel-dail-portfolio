96d0b892db01f8a1dd75b5e384bab3b22add65c6
israeldail <israeldail2@gmail.com>
2025-01-23 22:00:15 -0500

    fixed footer issue, might need to clean up styles since parent is using flex

--- portfolio/app/components/Footer.tsx
+++ portfolio/app/components/Footer.tsx
@@ -1,6 +1,6 @@
 function Footer() {
   return (
-    <div className="flex flex-col md:flex-row justify-around items-center p-4 bg-[#8e6d74] text-[#f5f5f5]">
+    <div className="flex flex-col md:flex-row justify-around items-center p-4 mt-auto bg-[#8e6d74] text-[#f5f5f5]">
       <div>links</div>
       <div>copyright</div>
       <div>social media</div>
